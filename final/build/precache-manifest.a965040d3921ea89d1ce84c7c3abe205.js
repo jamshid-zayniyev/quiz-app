3a0741224dc228bcd39c2918b59a95ab7d7d1f34
jamshid-zayniyev <jamshidzayniev10@gmail.com>
2023-09-11 12:34:44 +0500

    kmkl
    klm

---
+++ final/build/precache-manifest.a965040d3921ea89d1ce84c7c3abe205.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "182a2d99b0a4cd4363af8e4ca7ada2d2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5b5399fa6515e147075",
+    "url": "/static/css/main.19eca702.chunk.css"
+  },
+  {
+    "revision": "deacecf959496c1df5e7",
+    "url": "/static/js/2.45a5db28.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.45a5db28.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5b5399fa6515e147075",
+    "url": "/static/js/main.1cbbb682.chunk.js"
+  },
+  {
+    "revision": "69bbd7c1381a265f29c9",
+    "url": "/static/js/runtime-main.fda65720.js"
+  }
+]);
